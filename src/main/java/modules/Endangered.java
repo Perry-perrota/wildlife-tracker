2871d3226d620b8d7b3f815dd492bff6dc633054
Perry Obara <perryobara@gmail.com>
2019-07-07 23:45:31 +0300

    adds save and retrive methods

--- src/main/java/modules/Endangered.java
+++ src/main/java/modules/Endangered.java
@@ -1,5 +1,10 @@
 package modules;
 
+
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Endangered extends Animal {
     private int id;
     private String location;
@@ -40,5 +45,37 @@ public class Endangered extends Animal {
 
     public String getHealthStatus() {
         return healthStatus;
+    }
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Endangered)) {
+            return false;
+        } else {
+            Endangered newEndangered = (Endangered) otherAnimal;
+            return this.getName().equals(newEndangered.getName());
+        }
+    }
+        public void saveEndangered(){
+        try(Connection con=DB.sql2o.open()){
+            String sql="INSERT INTO animals (name,age,health,location,rangerName,type) VALUES (:name,:age,:health,:location,:rangerName,:type)";
+            this.id=(int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("age",this.age)
+                    .addParameter("health",this.healthStatus)
+                    .addParameter("location",this.location)
+                    .addParameter("rangerName",this.rangerName)
+                    .addParameter("type",this.type)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Endangered> allEndangered(){
+        String sql="SELECT * FROM animals";
+        try(Connection con=DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Endangered.class);
+        }
     }
 }
