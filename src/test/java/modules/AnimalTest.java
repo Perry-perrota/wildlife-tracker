d018038a125dba8c71effc2eb2c90f937285162d
Perry Obara <perryobara@gmail.com>
2019-07-07 22:08:04 +0300

    Adds test for finding animal by id

--- src/test/java/modules/AnimalTest.java
+++ src/test/java/modules/AnimalTest.java
@@ -1,9 +1,12 @@
 package modules;
 import org.junit.*;
-
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database=new DatabaseRule();
+
     @Test
     public void animal_instantiatesCorrectly_true(){
         Animal testAnimal=new Animal("lion");
@@ -25,6 +28,30 @@ public class AnimalTest {
         Animal testAnimal=new Animal("lion");
         testAnimal.saveAnimal();
         assertTrue(Animal.allAnimals().get(0).equals(testAnimal));
+    }
+    @Test
+    public void allAnimals_returnsAllInstancesOfAnimal_true(){
+        Animal firstAnimal=new Animal("lion");
+        firstAnimal.saveAnimal();
+        Animal secondAnimal=new Animal("wildebeest");
+        secondAnimal.saveAnimal();
+        assertEquals(true,Animal.allAnimals().get(0).equals(firstAnimal));
+        assertEquals(true,Animal.allAnimals().get(1).equals(secondAnimal));
+    }
+    @Test
+    public void saveAnimal_assignsIDtoObject(){
+        Animal testAnimal=new Animal("lion");
+        testAnimal.saveAnimal();
+        Animal savedAnimal=Animal.allAnimals().get(0);
+        assertEquals(testAnimal.getId(),savedAnimal.getId());
+    }
+    @Test
+    public void find_returnsAnimalWithSameId_secondAnimal(){
+        Animal firstAnimal=new Animal("lion");
+        firstAnimal.saveAnimal();
+        Animal secondanimal=new Animal("wildebeest");
+        secondanimal.saveAnimal();
+        assertEquals(Animal.find(secondanimal.getId()),secondanimal);
     }
 
 }
