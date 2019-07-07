1f44846aa1dc04488c172b535bc168e30afb056f
Perry Obara <perryobara@gmail.com>
2019-07-07 22:14:05 +0300

    adds find by method for finding by id functionality

--- src/main/java/modules/Animal.java
+++ src/main/java/modules/Animal.java
@@ -1,8 +1,18 @@
 package modules;
+import org.sql2o.*;
+
+import java.util.List;
 
 public class Animal {
     public String name;
     public String type;
+    private int id;
+
+    public static final  String ANIMAL_TYPE="notEndangered";
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
@@ -14,6 +24,7 @@ public class Animal {
 
     public Animal(String name){
         this.name=name;
+        this.type=ANIMAL_TYPE;
 
     }
     @Override
@@ -25,6 +36,36 @@ public class Animal {
             Animal newAnimal=(Animal) otherAnimal;
             return this.getName().equals(newAnimal.getName());
 
+        }
+    }
+    public void saveAnimal(){
+        try(Connection con=DB.sql2o.open()){
+            String sql="INSERT INTO animals (name,type) VALUES (:name,:type)";
+            this.id=(int) con.createQuery(sql, true)
+                    .throwOnMappingFailure(false)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Animal> allAnimals(){
+        String sql="SELECT * FROM animals";
+        try(Connection con=DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(Animal.class);
+        }
+
+    }
+    public static Animal find(int id){
+        try (Connection con=DB.sql2o.open()){
+            String sql="SELECT * FROM animals where id=:id";
+            Animal animal=con.createQuery(sql)
+                    .addParameter("id",id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(Animal.class);
+            return animal;
         }
     }
 }
