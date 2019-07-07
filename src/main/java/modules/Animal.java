f2e6f37006420ac61f0497784ac39d234821e557
Perry Obara <perryobara@gmail.com>
2019-07-07 11:11:18 +0300

    adds properties for the class and its getter methods as well as overrides the equals method

--- src/main/java/modules/Animal.java
+++ src/main/java/modules/Animal.java
@@ -1,4 +1,30 @@
 package modules;
 
 public class Animal {
+    public String name;
+    public String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Animal(String name){
+        this.name=name;
+
+    }
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        }
+        else{
+            Animal newAnimal=(Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+
+        }
+    }
 }
